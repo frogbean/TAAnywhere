9438112ac8b62030d602b1e43444b81cfa0a3822
frogbean <49106938+frogbean@users.noreply.github.com>
2023-09-06 15:56:03 +0100

    .

--- forge.config.js
+++ forge.config.js
@@ -1,17 +1,12 @@
 module.exports = {
   packagerConfig: {
     asar: true,
-    ignore: ["./downloads", "./temp"]
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
       config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
     },
     {
       name: '@electron-forge/maker-deb',
